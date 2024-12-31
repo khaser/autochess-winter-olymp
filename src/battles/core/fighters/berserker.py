11fa4136ca91c9ea5bc37fb636e01a2d17eab0e4
Andrey Ovchinnikov <s21v_ovchinnikov@179.ru>
2024-12-31 22:15:56 +0300

    fixes in berserker attack logic

--- src/battles/core/fighters/berserker.py
+++ src/battles/core/fighters/berserker.py
@@ -17,6 +17,22 @@ class Berserker(Unit):
         adx, ady = abs(self.pos[0] - fighter.pos[0]), abs(self.pos[1] - fighter.pos[1])
         return max(adx, ady) <= 1 and self.team != fighter.team and fighter.is_alive()
 
+    def perform_attack_phase(self, fighters, arrows):
+        attackable_fighters = []
+        minimal_hp = -1
+        for i in range(len(fighters)):
+            if self != fighters[i] and self.can_attack_enemy(fighters[i]):
+                attackable_fighters.append(i)
+                if minimal_hp == -1:
+                    minimal_hp = fighters[i].hp
+                minimal_hp = min(minimal_hp, fighters[i].hp)
+        for i in range(len(attackable_fighters) - 1, -1, -1):
+            if fighters[attackable_fighters[i]].hp != minimal_hp:
+                attackable_fighters.pop(i)
+        choosen = random.choice(attackable_fighters)
+        self.attack(fighters, arrows, choosen)
+        return not fighters[choosen].is_alive()
+
     def perform_turn(self, fighters, arrows, table_size):
         if self.can_attack(fighters):
             killed = self.perform_attack_phase(fighters, arrows)
